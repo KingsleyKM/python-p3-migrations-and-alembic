b99d6fb9813f504451bd7b606258c388c2ca355c
Kingsley Maina <mainakingsley7@gmail.com>
2023-12-10 19:18:35 +0300

    Done

---
+++ migrations/versions/cb5cc6855caf_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: cb5cc6855caf
+Revises: 
+Create Date: 2023-12-10 19:16:18.684017
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cb5cc6855caf'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
